8dd0f7ed9c1479f6878127a9c66b3e7020c92532
Rahul Kumar <rk32298@gmail.com>
2015-07-30 12:24:40 -0400

    Delete cli.ex

--- lib/universe/cli.ex
+++
@@ -1,8 +0,0 @@
-defmodule Universe.CLI do
-
-def parse_args(argv) do
-pares = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
-case parse do
-{[help: true],_,_} -> :help
-end
-end
