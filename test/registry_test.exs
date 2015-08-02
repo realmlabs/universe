236192ebd0dc12049355a194c2752b6e8fdf5c5c
Samantha Doran <samanthadoran3@gmail.com>
2015-08-02 06:09:36 -0400

    Make tests a touch more rigorous
    This should solve tests failing if we are being limited, though it ruins
    their accuracy

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,6 +1,7 @@
 defmodule Forwarder do
   use GenEvent
   use ExUnit.Case
+  import Poison.Parser
 
   def handle_event(event, parent) do
     send parent, event
@@ -21,9 +22,45 @@ defmodule Forwarder do
   end
 
   #Test using a known commit to make sure we are capable of getting information.
-  test "Fetch test" do
+  test "proper fetch test" do
     {status, response} = Universe.Registry.fetch(Universe.Registry.tree_url("realmlabs", "universe", "b43e7a02e4819e3802aed69140be5778379c006f"))
-    assert response.status_code === 200
+
+    case response.status_code do
+
+      #The sha of this commit should be the sha we passed in
+      200 ->
+        assert status === :ok
+        content = parse!(response.body, keys: :atoms).sha
+        assert content === "b43e7a02e4819e3802aed69140be5778379c006f"
+
+      #We know this commit exists, it should only fail if we are being limited
+      403 ->
+        IO.puts("WARN: Requests have been blocked, results of proper fetch test may be innacurate!")
+        content = parse!(response.body, keys: :atoms).documentation_url
+        assert content === "https://developer.github.com/v3/#rate-limiting"
+    end
+  end
+
+  #This should 404
+  test "Bad url" do
+    {status, response} = Universe.Registry.fetch(Universe.Registry.tree_url("", "", ""))
+
     assert status === :ok
+
+    #This should never :ok, the url is bad
+    case response.status_code do
+      200 ->
+        assert false
+
+      #This should either 403 because it doesn't exist/is invisible, or because we are limited.
+      403 ->
+        content = parse!(response.body, keys: :atoms)
+        assert (content.message === "Not Found" or content.documentation_url === "https://developer.github.com/v3/#rate-limiting")
+
+      #Seems to be reserved for malformed requests
+      404 ->
+        content = parse!(response.body, keys: :atoms).message
+        assert content === "Not Found"
+    end
   end
 end
