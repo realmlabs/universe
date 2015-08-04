1a5a0326a3660fbc2d52449a5d6526e04ba7be89
ejgbadebo <exg1977@rit.edu>
2015-08-04 19:24:02 -0400

    Added basic/placeholder functions in hex.ex.

--- lib/universe/hex.ex
+++ lib/universe/hex.ex
@@ -1,5 +1,6 @@
 defmodule Universe.Hex do
 	use GenServer
+	
 
   def start_link(event_manager, opts \\ []) do
 		GenServer.start_link(__MODULE__, event_manager, opts)
@@ -8,7 +9,56 @@ defmodule Universe.Hex do
   def clone(package) do
     GenServer.call __MODULE__, {:clone, package}
   end
+  
+  @doc ~S"""
+  Returns a URL for a specified hex.pm package and version.
 
+  ## Example:
+  
+  	iex> Universe.Hex.get_hex_url("httpoison","0.7.3")
+	"https://s3.amazonaws.com/s3.hex.pm/tarballs/httpoison-0.7.3.tar"
+
+  """
+  def get_hex_url(package, ver) do
+    "https://s3.amazonaws.com/s3.hex.pm/tarballs/#{package}-#{ver}.tar"
+  end
+
+  #placeholder functions-------------------
+
+  @doc ~S"""
+
+  Get a URL with specified parameters.
+
+  """
+  def fetch(url, opts \\ %{}) do
+    HTTPoison.get(url, [], params: opts)
+  end
+
+  @doc ~S"""
+
+  Get a specified package from hex.pm.
+
+  """
+  def get_package(package) do
+  end
+
+
+  @doc ~S"""
+
+  Get dependencies of a package recursively.
+
+  """
+  #If no parameters, do nothing.
+  def get_deps([]) do
+  end
+  
+  #If dependencies are remaining download first in the list. 
+  def get_deps([head|tail]) do
+    get_deps(tail)
+  end
+
+
+  #----------------------------------------
 
   def handle_call({:clone, package}, _from, _state) do
     {:reply, {:ok}, []}
